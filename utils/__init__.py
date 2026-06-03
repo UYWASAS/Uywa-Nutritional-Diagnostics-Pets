af03824d88d36728ca3bfcd5247466989031a79c
UYWASAS <uywasas@gmail.com>
2026-06-03 11:38:04 -0500

    Create __init__.py

---
+++ utils/__init__.py
@@ -0,0 +1,3 @@
+# Exporta referencias principales y herramientas de formato
+from .nutrient_reference import NUTRIENTES_REFERENCIA_PERRO, NUTRIENTES_REFERENCIA_GATO
+from .fmt_tools import fmt2, fmt2_df
