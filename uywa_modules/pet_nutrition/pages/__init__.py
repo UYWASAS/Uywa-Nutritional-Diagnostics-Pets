a842a6f1d9a6efeeb8d45e209a2bfb4db7d60c71
UYWASAS <uywasas@gmail.com>
2026-07-23 16:29:32 -0500

    Update __init__.py

--- uywa_modules/pet_nutrition/pages/__init__.py
+++ uywa_modules/pet_nutrition/pages/__init__.py
@@ -1,5 +1,6 @@
 from .analysis_page import show_analysis_page
 from .compare_page import show_compare_page
+from .followup_page import show_followup_page
 from .profile_page import (
     get_estado_corporal,
     show_profile_page,
@@ -8,8 +9,9 @@ from .summary_page import show_summary_page
 
 __all__ = [
     "get_estado_corporal",
-    "show_profile_page",
     "show_analysis_page",
     "show_compare_page",
+    "show_followup_page",
+    "show_profile_page",
     "show_summary_page",
 ]
