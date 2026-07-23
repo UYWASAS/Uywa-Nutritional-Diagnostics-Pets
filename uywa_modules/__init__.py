4286a62879c9e037e9f5b94c3a91d2de2c129b37
UYWASAS <uywasas@gmail.com>
2026-07-23 10:38:53 -0500

    Update __init__.py

--- uywa_modules/__init__.py
+++ uywa_modules/__init__.py
@@ -1,1 +1,9 @@
+from uywa_modules.pet_nutrition import (
+    build_platform_user_adapter,
+    render_pet_nutrition,
+)
 
+__all__ = [
+    "build_platform_user_adapter",
+    "render_pet_nutrition",
+]
