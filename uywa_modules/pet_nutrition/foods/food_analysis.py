d8f95866a5b50dfe2bdbc5842294166c239bd2d1
UYWASAS <uywasas@gmail.com>
2026-07-23 16:51:06 -0500

    Update food_analysis.py

--- uywa_modules/pet_nutrition/foods/food_analysis.py
+++ uywa_modules/pet_nutrition/foods/food_analysis.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 import pandas as pd
 import streamlit as st
 
-from uywa_modules.pet_nutrition.foods.food_database import (
+from .food_database import (
     FOODS,
     calculate_energy,
     calculate_ena,
