25f0ca84c41f7c8e7123a9006635d2331a7fd9be
UYWASAS <uywasas@gmail.com>
2026-07-23 16:54:24 -0500

    Update __init__.py

--- uywa_modules/pet_nutrition/pages/__init__.py
+++ uywa_modules/pet_nutrition/pages/__init__.py
@@ -1,29 +1,20 @@
-from .food_analysis import (
-    build_energy_breakdown_table_with_edits,
-    plot_energy_breakdown_stacked_with_edits,
-    show_food_analysis,
+from .analysis_page import show_analysis_page
+from .compare_page import show_compare_page
+from .followup_page import show_followup_page
+from .profile_page import (
+    get_estado_corporal,
+    show_profile_page,
 )
-
-from .food_database import (
-    FOODS,
-    calculate_ena,
-    calculate_energy,
-    calculate_energy_breakdown,
-    food_data_is_precalculated,
-    get_food_data,
-    get_food_names,
-    infer_me_from_food_manufacturer_reference,
-    infer_me_from_manufacturer_reference,
-)
+from .summary_page import show_summary_page
 
 __all__ = [
-    "FOODS",
-    "calculate_ena",
-    "calculate_energy",
-    "calculate_energy_breakdown",
-    "food_data_is_precalculated",
-    "get_food_data",
-    "get_food_names",
+    "get_estado_corporal",
+    "show_analysis_page",
+    "show_compare_page",
+    "show_followup_page",
+    "show_profile_page",
+    "show_summary_page",
+]
     "infer_me_from_food_manufacturer_reference",
     "infer_me_from_manufacturer_reference",
     "show_food_analysis",
