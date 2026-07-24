c1c3ef36d16a02b4db13507bf59007f558d0f814
UYWASAS <uywasas@gmail.com>
2026-07-24 07:53:49 -0500

    Update pet_nutrition.py

--- uywa_modules/pet_nutrition/pet_nutrition.py
+++ uywa_modules/pet_nutrition/pet_nutrition.py
@@ -11,9 +11,7 @@ from utils.ui_components import (
 )
 from utils.ui_theme import inject_uywa_theme
 
-def show_analysis_page():
-    import streamlit as st
-    st.write("Analysis OK")
+from .pages.analysis_page import show_analysis_page
 from .pages.compare_page import show_compare_page
 from .pages.followup_page import show_followup_page
 from .pages.profile_page import show_profile_page
